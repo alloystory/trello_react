e988bd7ac935be81400bf2bcd381218b68e70264
Aloysius <alloystory@gmail.com>
2020-12-23 23:07:40 +0800

    edit seed data

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,130 +2,47 @@ import React, { Component } from "react";
 import List from "./List";
 import { nanoid } from "nanoid";
 
+const SEED_DATA = [
+  {
+    title: "List 1",
+    cards: ["Content 1", "Content 2", "LOOOOOOOOOOOOOOOOOONG Content 3"],
+  },
+  {
+    title: "List 2",
+    cards: ["Content 1", "LOOOOOOOOOOOOOOOOOONG Content 3"],
+  },
+  {
+    title: "List 3",
+    cards: [
+      "Content 1",
+      "Content 2",
+      "LOOOOOOOOOOOOOOOOOONG Content 3",
+      "Some Content 4",
+    ],
+  },
+];
+
 class Board extends Component {
   state = {
-    // lists: [
-    //   {
-    //     title: "List 1",
-    //     cards: [
-    //       { content: "Some Card Content" },
-    //       { content: "More content" },
-    //       { content: "A very looooooooooooooooooooooooooooooooong content" },
-    //     ],
-    //   },
-    //   {
-    //     title: "List 2",
-    //     cards: [
-    //       { content: "Some Card Content" },
-    //       { content: "A very looooooooooooooooooooooooooooooooong content" },
-    //     ],
-    //   },
-    //   {
-    //     title: "List 3",
-    //     cards: [
-    //       { content: "More content" },
-    //       { content: "A very looooooooooooooooooooooooooooooooong content" },
-    //     ],
-    //   },
-    //   {
-    //     title: "List 4",
-    //     cards: [{ content: "Some Card Content" }, { content: "More content" }],
-    //   },
-    //   {
-    //     title: "List 5",
-    //     cards: [
-    //       { content: "Some Card Content" },
-    //       { content: "More content" },
-    //       { content: "A very looooooooooooooooooooooooooooooooong content" },
-    //       { content: "A very HAHHAHAHAA content" },
-    //     ],
-    //   },
-    // ],
-    lists: [
-      {
-        id: 1,
-        title: "List 1",
-        cards: [
-          {
-            id: 1,
-            content: "Some Card Content",
-          },
-          {
-            id: 2,
-            content: "More content",
-          },
-          {
-            id: 3,
-            content: "A very looooooooooooooooooooooooooooooooong content",
-          },
-        ],
-      },
-      {
-        id: 2,
-        title: "List 2",
-        cards: [
-          {
-            id: 1,
-            content: "Some Card Content",
-          },
-          {
-            id: 2,
-            content: "A very looooooooooooooooooooooooooooooooong content",
-          },
-        ],
-      },
-      {
-        id: 3,
-        title: "List 3",
-        cards: [
-          {
-            id: 1,
-            content: "More content",
-          },
-          {
-            id: 2,
-            content: "A very looooooooooooooooooooooooooooooooong content",
-          },
-        ],
-      },
-      {
-        id: 4,
-        title: "List 4",
-        cards: [
-          {
-            id: 1,
-            content: "Some Card Content",
-          },
-          {
-            id: 2,
-            content: "More content",
-          },
-        ],
-      },
-      {
-        id: 5,
-        title: "List 5",
-        cards: [
-          {
-            id: 1,
-            content: "Some Card Content",
-          },
-          {
-            id: 2,
-            content: "More content",
-          },
-          {
-            id: 3,
-            content: "A very looooooooooooooooooooooooooooooooong content",
-          },
-          {
-            id: 4,
-            content: "A very HAHHAHAHAA content",
-          },
-        ],
-      },
-    ],
+    lists: [],
   };
+
+  componentDidMount() {
+    const lists = [];
+
+    for (const list of SEED_DATA) {
+      const newList = { id: nanoid(), title: list.title, cards: [] };
+      for (const card of list.cards) {
+        newList.cards.push({
+          id: nanoid(),
+          content: card,
+        });
+      }
+      lists.push(newList);
+    }
+
+    this.setState(() => ({ lists: lists }));
+  }
 
   handleAddList = () => {
     this.setState((state) => ({
