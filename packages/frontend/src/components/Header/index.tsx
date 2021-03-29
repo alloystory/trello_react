259a141faef92f0d3ca38524e70f09388d6c8d65
aloysius <alloystory@gmail.com>
2021-03-29 22:53:49 +0800

    edited header

--- packages/frontend/src/components/Header/index.tsx
+++ packages/frontend/src/components/Header/index.tsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { ReactComponent as SunIcon } from '../../assets/sun.svg'
-import styles from './index.module.css'
+import Button from '../Button'
+import styles from './index.module.scss'
+
+function BoardTitle() {
+  return <h1 className={styles.boardTitle}>Board Title</h1>
+}
+
+function SearchBar() {
+  return (
+    <input type="text" className={styles.searchBar} placeholder="Search..." />
+  )
+}
+
+function RightButtons() {
+  return (
+    <div className={styles.rightButtons}>
+      <Button isSquare>
+        <SunIcon className={styles.sunIcon} />
+      </Button>
+      <Button isSquare>AC</Button>
+    </div>
+  )
+}
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <h1 className="text-heading">Board Title</h1>
-      <input
-        type="text"
-        className={styles['header--search']}
-        placeholder="Search..."
-      />
-      <div className={styles['header--btns']}>
-        <button className="btn header--btn">
-          <SunIcon className={styles['header--theme-icon']} />
-        </button>
-        <button className="btn header--btn">AC</button>
-      </div>
+      <BoardTitle />
+      <SearchBar />
+      <RightButtons />
     </div>
   )
 }
